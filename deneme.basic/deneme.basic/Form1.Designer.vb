bf4deb7299fbc5af2e0b137c4c3752f1e21c2d20
i5-7200u <81111892+i5-7200u@users.noreply.github.com>
2021-11-08 21:23:30 +0300

    Add files via upload

---
+++ deneme.basic/deneme.basic/Form1.Designer.vb
@@ -0,0 +1,87 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form, bileşen listesini temizlemeyi bırakmayı geçersiz kılar.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows Form Tasarımcısı tarafından gerektirilir
+    Private components As System.ComponentModel.IContainer
+
+    'NOT: Aşağıdaki yordam Windows Form Tasarımcısı için gereklidir
+    'Windows Form Tasarımcısı kullanılarak değiştirilebilir.  
+    'Kod düzenleyicisini kullanarak değiştirmeyin.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        resources.ApplyResources(Me.Button1, "Button1")
+        Me.Button1.Name = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        resources.ApplyResources(Me.Label1, "Label1")
+        Me.Label1.Name = "Label1"
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 10
+        '
+        'Panel1
+        '
+        resources.ApplyResources(Me.Panel1, "Panel1")
+        Me.Panel1.BackColor = System.Drawing.Color.Black
+        Me.Panel1.Controls.Add(Me.Panel2)
+        Me.Panel1.Name = "Panel1"
+        '
+        'Panel2
+        '
+        resources.ApplyResources(Me.Panel2, "Panel2")
+        Me.Panel2.Name = "Panel2"
+        '
+        'Timer2
+        '
+        Me.Timer2.Interval = 60
+        '
+        'Form1
+        '
+        resources.ApplyResources(Me, "$this")
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Panel1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents Timer2 As Timer
+End Class
---
+++ deneme.basic/deneme.basic/Form1.vb
@@ -0,0 +1,53 @@
+﻿Imports System.Globalization
+Imports Microsoft.VisualBasic.Devices
+Imports System.ComponentModel
+Imports System.IO
+Imports IWshRuntimeLibrary
+Public Class Form1
+    Public qwj As Integer = 0
+    Dim huh As Integer = 0
+    Public a As String = "ardakc"
+    Declare Function SetProcessWorkingSetSize Lib "kernel32.dll" (ByVal process As IntPtr, ByVal minimumworkingsetsize As Integer, ByVal maximumworkingsetsize As Integer) As Integer
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If Not Button1.Text.Contains("Durdur") Then
+            Timer1.Start()
+            Timer2.Start()
+            Button1.Text = "Durdur"
+        Else
+            Timer1.Stop()
+            Timer2.Stop()
+            Panel1.BackColor = Color.Black
+            Panel2.BackColor = Color.Black
+            Button1.Text = "Baslat"
+        End If
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Dim qwe As Color() = {Color.Red, Color.Green, Color.Blue, Color.Black, Color.White, Color.YellowGreen, Color.GreenYellow, Color.DarkBlue, Color.Gold, Color.Yellow, Color.Aqua, Color.DarkRed, Color.LightSeaGreen, Color.DarkGray, Color.DarkSlateGray, Color.DimGray, Color.LightSlateGray, Color.LightGray, Color.SlateGray}
+        Dim ewq As New Random()
+        Panel1.BackColor = qwe(ewq.Next(0, qwe.Length))
+        Panel2.BackColor = qwe(ewq.Next(0, qwe.Length))
+    End Sub
+
+    Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles Timer2.Tick
+        If Timer1.Enabled = True Then
+            If Not Button1.Text.Contains("60") Then
+                For gug As Integer = 0 To 60
+                    huh = huh + 1
+                Next
+            Else
+                Timer1.Stop()
+                Timer2.Stop()
+                Panel1.BackColor = Color.Black
+                Panel2.BackColor = Color.Black
+                MsgBox("you are epilepsy")
+            End If
+            Dim qwwe As Integer = huh / 1000
+            Button1.Text = "Durdur ( " & Convert.ToString(qwwe) & " )"
+            If Not Timer1.Enabled = True Then
+                Timer2.Stop()
+                Button1.Text = "Baslat"
+            End If
+        End If
+    End Sub
+End Class
